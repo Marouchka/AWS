1ca18a858e0db53cd4085f2b7b3b1cada1591e3b
Marouchka <trebeheiram@gmail.com>
2017-06-23 11:46:19 -0400

    AWS_1.0 Arduino sketck uploaded
    Variables for pin assignment
    Wait time uses "delay()" function

---
+++ AWS_1.0/AWS_1.0.ino
@@ -0,0 +1,33 @@
+// int sensorPin = 5; 
+const byte sensorPin = A2;
+// int pumpPin = 2;
+const byte pumpPin = A1;
+bool flagPump = true;
+int threshold = 800;
+
+void setup() 
+{
+  pinMode(sensorPin, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(pumpPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+digitalWrite(sensorPin, HIGH);
+delay(1000);
+int sensorIn = analogRead(A0);
+Serial.println(sensorIn);
+digitalWrite(sensorPin, LOW);
+
+if(flagPump)
+{
+  if(sensorIn > threshold)digitalWrite(pumpPin, HIGH);
+  else digitalWrite(pumpPin, LOW);
+}
+
+if(digitalRead(pumpPin)) delay(2000);
+else delay(10000);
+
+}
